16a9a2ffa9023000d33a17a8c28e49dbeadad7ce
YIHYUN HA <hihyun16@gmail.com>
2025-09-25 21:44:55 +0900

    feat: user session 조회 방식 및 데이터 구조 변경
    - http session에서 다이렉트로 조회하는 대신 security context에서 조회하도록 변경
    - user session 데이터에 userId 값을 추가
    
    Issue #8

--- src/main/kotlin/com/yhproject/mywiki/auth/SessionUser.kt
+++ src/main/kotlin/com/yhproject/mywiki/auth/SessionUser.kt
@@ -4,8 +4,8 @@ import com.yhproject.mywiki.domain.user.User
 import java.io.Serializable
 
 data class SessionUser(
-    val name: String,
-    val email: String
+    val id: Long,
+    val name: String
 ) : Serializable {
-    constructor(user: User) : this(user.name, user.email)
+    constructor(user: User) : this(user.id, user.name)
 }
--- src/main/kotlin/com/yhproject/mywiki/config/SecurityConfig.kt
+++ src/main/kotlin/com/yhproject/mywiki/config/SecurityConfig.kt
@@ -3,6 +3,7 @@ package com.yhproject.mywiki.config
 import com.yhproject.mywiki.auth.CustomOAuth2UserService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
@@ -10,24 +11,35 @@ import org.springframework.security.web.SecurityFilterChain
 @Configuration
 @EnableWebSecurity
 class SecurityConfig(
-    private val customOAuth2UserService: CustomOAuth2UserService
+    private val customOAuth2UserService: CustomOAuth2UserService,
 ) {
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http
             .csrf { it.disable() }
-            .authorizeHttpRequests { auth ->
-                auth
+            .headers { it.frameOptions { frameOptions -> frameOptions.disable() } }
+            .authorizeHttpRequests {
+                it
                     .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/error").permitAll()
-                    .requestMatchers("/api/v1/**").hasRole("USER")
+                    .requestMatchers("/api/**").hasRole("USER")
                     .anyRequest().authenticated()
             }
-            .logout { logout -> logout.logoutSuccessUrl("/") }
-            .oauth2Login { oauth2 ->
-                oauth2.userInfoEndpoint { userInfo ->
-                    userInfo.userService(customOAuth2UserService)
+            .logout { it.logoutSuccessUrl("/") }
+            .oauth2Login {
+                it.userInfoEndpoint {
+                    it.userService(customOAuth2UserService)
                 }
+            }
+            .exceptionHandling { handler ->
+                handler.defaultAuthenticationEntryPointFor(
+                    { _, response, _ ->
+                        response.status = HttpStatus.UNAUTHORIZED.value()
+                    },
+                    { request ->
+                        request.requestURI.startsWith("/api/")
+                    }
+                )
             }
 
         return http.build()

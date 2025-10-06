462824c9430434e16472b156fbc620c6b70d71f7
YIHYUN HA <hihyun16@gmail.com>
2025-10-06 10:35:30 +0900

    feat: 메인 페이지 추가
    Issue #7

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import {BrowserRouter as Router, Navigate, Route, Routes} from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import MainPage from "./pages/MainPage";
+import {AuthProvider} from "./contexts/AuthContext";
+import PrivateRoute from "./components/common/PrivateRoute";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="*" element={<Navigate to="/login" />} />
-      </Routes>
-    </Router>
+      <Router>
+          <AuthProvider>
+              <Routes>
+                  <Route path="/" element={<PrivateRoute><MainPage /></PrivateRoute>} />
+                  <Route path="/login" element={<LoginPage />} />
+                  <Route path="*" element={<Navigate to="/" />} />
+              </Routes>
+          </AuthProvider>
+      </Router>
   );
 }
 
